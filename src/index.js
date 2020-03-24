031f1d49cd3d9f1db5d02d4ab6e8cae98c4d406f
zhoutianyi <tianyiz8@illinois.edu>
2020-03-24 11:25:25 -0500

    fifth

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'), routing
 );
 
 // If you want your app to work offline and load faster, you can change
