8440c43a45a15c74cd51cf69ed35117b2a2c358d
zhoutianyi <tianyiz8@illinois.edu>
2020-03-24 00:34:02 -0500

    final

--- src/index.js
+++ src/index.js
@@ -23,11 +23,7 @@ const routing = () => (
     </Switch> 
   </BrowserRouter>
 )
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(routing, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
