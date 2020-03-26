e901d8687f4646ec6d1a937c2a1c06efeaecfa22
zhoutianyi <tianyiz8@illinois.edu>
2020-03-25 19:36:00 -0500

    finally

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Particle from './component/particle/particle'
 import {Layout, Header,Navigation,Drawer,Content} from 'react-mdl';
 import Main from './pages/main'
 import Footer from './component/footer/footer';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Landing from './pages/landingpage/landingpage';
 import AboutMe from './pages/aboutme/aboutme';
 import Project from './pages/project/project';
@@ -13,18 +13,18 @@ import Resume from './pages/resume/resume';
 function App() {
   return (
     <div style={{height: '800px', position: 'relative'}}>
+    <Router basename={ process.env.PUBLIC_URL + '/' }>
     <Layout fixedHeader>
         <Header className = "header-color" transparent title={<span><span style={{ color: '#ddd', marginBottom: '0'}}></span><strong>Navigation</strong></span>}>
           <Navigation>
-            <a href="/landing">Profile</a>
-            <a href="/aboutMe">About Me</a>
-            <a href="/resume">Resume</a>
-            <a href="/project">Project</a>
-            <a href="/contact">Contact</a>
+            <Link to="/">Profile</Link>
+            <Link to="/aboutMe">About</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/project">Project</Link>
+            <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
         <Drawer title="Go to ...">
-         <Router basename={ process.env.PUBLIC_URL + '/' }>
             <Navigation>
               <Link to="/">Profile</Link>
               <Link to="/aboutMe">About</Link>
@@ -32,15 +32,22 @@ function App() {
               <Link to="/project">Project</Link>
               <Link to="/contact">Contact</Link>
             </Navigation>
-          </Router>
         </Drawer>
         <Content>
           <div className="page-content" />
           <Particle />
-          <Main />
+          <Switch> 
+              <Route exact path= '/' component={Landing} />
+              <Route path= '/aboutMe' component={AboutMe} />
+              <Route path= '/contact' component={Contact} />
+              <Route path= '/project' component={Project} />
+              <Route path= '/resume' component={Resume} />
+              <Route path= '/landing' component={Landing} />
+            </Switch> 
         </Content>
         <Footer/>
     </Layout>
+    </Router>
 </div>
   );
 }
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './landingpage/landingpage';
 import AboutMe from './aboutme/aboutme';
 import Project from './project/project';
@@ -8,14 +8,14 @@ import Resume from './resume/resume';
 
 const Main = () => (
   <BrowserRouter basename={ process.env.PUBLIC_URL + '/' }>
-    <div> 
+    <Switch> 
       <Route exact path= '/' component={Landing} />
       <Route path= '/aboutMe' component={AboutMe} />
       <Route path= '/contact' component={Contact} />
       <Route path= '/project' component={Project} />
       <Route path= '/resume' component={Resume} />
       <Route path= '/landing' component={Landing} />
-    </div> 
+    </Switch> 
   </BrowserRouter>
 )
 
